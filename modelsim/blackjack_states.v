a584285cdc3095e5fcd4a36c5a653dabd8e3c3d8
miguel <argolo.mb@gmail.com>
2019-12-02 12:18:04 -0300

    Output Parcial

--- modelsim/blackjack_states.v
+++ modelsim/blackjack_states.v
@@ -1,23 +1,40 @@
 module blackjack_states
 (
-	input	clk, reset, hit, stay;
-	output card_out;
+	input	clk, reset, hit, stay,
+	output card_out,mem_ctrl_rd, mem_ctrl_wr
 	
-	input reg  [7:0] card_in;
-	output reg [5:0] card_ctrl;
+	input [15:0] card_in,
+	output reg [5:0] card_ctrl,
 
-	output reg [7:0] player_hand;
-	output reg [7:0] dealer_hand;
+	output reg [7:0] player_hand,
+	output reg [7:0] dealer_hand,
 
-	output reg [7:0] player_action;
-	output reg [7:0] dealer_action;
+	output reg [7:0] player_action,
+	output reg [7:0] dealer_action,
 
-	output reg [7:0] win, lose, tie;
+	output reg [7:0] win, lose, tie
 );
 	// Declare state register
-	reg		[2:0]state;
+	reg	[2:0]state;
+	reg [5:0]mem_addr
+	reg [4:0]sum_d, sum_p
 
+	reg bj_p, bj_d, burst_p, burst_d, has_ace_p,twone_p,stay_d, mix_cards, ;
+
+
+    wire shuffle_ok;
+    wire [5:0] card_ctrl;
+    wire [7:0] card;
 	// Declare states
+deck deck_0(
+		.clk(clk) ,
+		.reset(reset) ,
+		.mix_cards(mix_cards),
+    	.card_ctrl(card_ctrl),        
+    	.card(card)
+		.shuffle_ok(shuffle_ok)
+
+);
 	
 	//PL_CARDx == PLAYER CARD; DL_CARDx=DEALER CARD; PL_TURN=PLAYER TURN;DL_TURN=DEALER TURN
 	
@@ -32,82 +49,124 @@ module blackjack_states
 		else
 			case (state)
 				START:
+					if(shuffle_ok)
 					begin
 						state <= PL_CARD1;
 					end
+				else
+					begin
+					state<=START  
+					end
 				PL_CARD1:
-					begin
-						state <= DL_CARD1;
-					end
+					state <= DL_CARD1;
+				
 				DL_CARD1:
-					begin
-						state <= PL_CARD2;
-					end
+					state <= PL_CARD2;
+				
 				PL_CARD2:
-					begin
-						state <= DL_CARD2;
-					end
+					state <= DL_CARD2;
+		
 				DL_CARD2: //GAME OR PL_TURN 
-					begin
-						state <= GAME;
-					end
+					if(bj_d || bj_p)
+						begin
+							state<=GAME;
+						end
+					else
+						begin
+							state <= PL_TURN;
+						end
 				PL_TURN: //GAME OR DL_TURN OR PL_TURN
-					begin
-						state <= S1;
-					end
+					if(burst_p && !has_ace_p)
+						begin
+							state<=GAME;
+						end
+					else
+						begin
+							state<=PL_TURN;
+						end
+					if(stay || twone_p)
+						begin
+							state<=DL_TURN;
+						end
+					else
+						begin
+							state<=PL_TURN;
+						end
+				
 				GAME: //STAY HERE TO MAKE MATH 
-					begin
-						state <= GAME;
-					end
+					state <= GAME;
+				
 				DL_TURN: ////GAME OR DL_TURN OR PL_TURN
-					begin
-						state <= S1;
-					end					
+					if(stay_d || burst_d)
+						begin
+							state<=GAME;
+						end
+					else
+						begin
+							state<=DL_TURN;
+						end
 			endcase
 	end
 
 	// Determine the output based only on the current state
 	// and the input (do not wait for a clock edge).
-	always @ (state)
+	always @ (posedge clk)
 	begin
 		case (state)
 			START: // START DECK 
+				mix_cards <= 1;	
+				mem_addr <= 0;
+				mem_ctrl_rd <= 1;
+				sum_p<=0;
+				sum_d<=0;
+				has_ace_p<=0
+
+			PL_CARD1: //GET A CARD				
+				if(card_in == 11)
 				begin
-					state <= PL_CARD1;
+					sum_p<=sum_p+card_in-1;
+					has_ace_p<=1;
+					mem_addr<=mem_addr+1
 				end
-			PL_CARD1: //GET A CARD
-				begin
-					state <= DL_CARD1;
-				end
+				else
+					begin
+					sum_p<=sum_p+card_in;
+					mem_addr<=mem_addr+1
+					end
+				
 			DL_CARD1: //GET A CARD
-				begin
-					state <= PL_CARD2;
-				end
+				if(card_in == 11)
+					begin
+						sum_p<=sum_p+card_in-1;
+						has_ace_p<=1;
+						mem_addr<=mem_addr+1
+					end
+				else
+					begin
+						sum_p<=sum_p+card_in;
+						mem_addr<=mem_addr+1
+					end
+					
+				
 			PL_CARD2: //GET A CARD **** SET PLAYER HAND **** 
-				begin
-					state <= DL_CARD2;
-				end
+				
+					
+				
 			DL_CARD2: //GET A CARD **** SET DEALER HAND
-				begin
-					state <= GAME;
-				end
+			
+					
+			
 			PL_TURN: //WAIT FOR HIT OR STAY BUTTON IF SUM < 21 ELSE BURST_PLAYER **** DEALER WON
-				begin
-					state <= S1;
-				end
+			
 			 /*
 			 TIE IF PLAYER_HAND == DEALER_HAND OR BJ_P AND BJ_D OR BURST_PLAYER
 			 LOSE IF BURST_PLAYER OR BLACKJACK_DEALER OR PLAYER_HAND<DEALER_HAND
 			 WIN IF PLAYER_HAND>DEALER_HAND OR BLACKJACK_P OR BURST_DEALER			 
 			 */
 			GAME:
-				begin
-					state <= GAME;
-				end
+
 			DL_TURN: ////WAIT FOR HIT OR STAY BUTTON IF SUM < 21 ELSE BURST_DEALER **** PLAYER WON
-				begin
-					state <= S1;
-				end					
+			
 		endcase
 	end
 
